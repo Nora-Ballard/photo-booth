4523a51d8aa46f13966e4f5a0591422b8073aa53
Nora Ballard <4589305+Nora-Ballard@users.noreply.github.com>
2018-04-27 19:24:29 -0500

    Bug fixes

--- gui.py
+++ gui.py
@@ -3,40 +3,40 @@ from picamera import PiCamera
 import time 
 
 ApplicationTheme =  {
-    'background':'#B8A3CD',
-    'foreground':'#F8F8F2',
+    "background":"#B8A3CD",
+    "foreground":"#F8F8F2",
 }
-
 class Application(tk.Frame):
-    def __init_(self, master=None):
+    def __init__(self, master=None):
         super().__init__(master)
         self.pack()
         self.create_widgets()
-        self.photo_path = '/home/pi/Desktop/PhotoBooth/image.png'
+        self.config = {
+            "photo_path" : "/home/pi/Desktop/PhotoBooth/image.jpg",
+        }
 
     def create_widgets(self):
-        self.shutter = tk.Button(self)
-        self.shutter['test'] = 'Take Picture'
-        self.shutter['command'] = self.shutter_press()
-        self.shutter.pack(side=top)
+        self.shutterBtn = tk.Button(self)
+        self.shutterBtn["test"] = "Take Picture"
+        self.shutterBtn["command"] = self.shutter_press
+        self.shutterBtn.pack(side="top")
 
-    def shutter_press()
+    def shutter_press(self):
         camera = PiCamera()
 
         camera.start_preview()
         time.sleep(5)
 
-        camera.capture(self.photo_path, format="jpg")
+        camera.capture(self.config["photo_path"], format="jpg")
         camera.stop_preview()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     root = tk.Tk()
-    root.wm_title('Photo Booth')
+    root.wm_title("Photo Booth")
     root.configure(
-        bg=ApplicationTheme['background'], 
-        fg=ApplicationTheme['foreground'],
+        bg=ApplicationTheme["background"],
     )
     
-    Window(root)
+    app = Application(master=root)
     root.mainloop()
